25dfc6c21697ddc6a8ed8e1452a272e00cc69705
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:50:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "energyconsumpmonito-45952",
+  url: "https://energyconsumpmonito-45952.botics.co",
+  api: "https://energyconsumpmonito-45952.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
